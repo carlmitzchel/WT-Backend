c2e80c244cf3d975459a7ee6f4c01602ac832b1f
Carl Mitzchel Padua <carlmitzchel@gmail.com>
2024-09-23 20:24:50 +0800

    Create idRand.ts

---
+++ app/utils/idRand.ts
@@ -0,0 +1,7 @@
+export const idRand = (length: number) => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  return Array.from({ length }, () =>
+    characters.charAt(Math.floor(Math.random() * characters.length))
+  ).join("");
+};
